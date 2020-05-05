37660342aec98aaff01322eb0a2ff1f5d69a97ae
Matheus Oliveira da Silva <matheus.oliveira.silv@gmail.com>
2020-05-04 21:19:59 -0300

    Change view of series listing, improve design and layout

--- resources/views/series/index.php
+++ resources/views/series/index.php
@@ -5,12 +5,21 @@
     <meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, user-scalable=0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Series Control</title>
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
 </head>
 
 <body>
-    <ul>
-    <?php foreach ($series as $serie): ?>
-    <li><?= $serie; ?></li>
-    <?php endforeach; ?>
+    <div class="container">
+        <div class="jumbotron">
+            <h1>Series</h1>
+	</div>
+
+	<a href="#" class="btn btn-dark mb-2">Add</a>
+
+        <ul class="list-group">
+        <?php foreach ($series as $serie): ?>
+        <li class="list-group-item"><?= $serie; ?></li>
+        <?php endforeach; ?>
+    </div>
 </body>
 </html>
