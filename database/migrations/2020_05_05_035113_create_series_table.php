5d4908bf3de41e2f174de1bd9f37c71364acbbf8
Matheus Oliveira da Silva <matheus.oliveira.silv@gmail.com>
2020-05-05 01:34:22 -0300

    Create migrations

---
+++ database/migrations/2020_05_05_035113_create_series_table.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateSeriesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('series', function (Blueprint $table) {
+            $table->string('name');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('series');
+    }
+}
