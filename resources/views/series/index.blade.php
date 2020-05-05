b9ff650447c791de842509fe686306f35382acaf
Matheus Oliveira da Silva <matheus.oliveira.silv@gmail.com>
2020-05-05 09:37:44 -0300

    Refactor foreach body to remove php tag

--- resources/views/series/index.blade.php
+++ resources/views/series/index.blade.php
@@ -10,7 +10,7 @@ Series
 
 <ul class="list-group">
 @foreach ($series as $serie)
-<li class="list-group-item"><?= $serie; ?></li>
+<li class="list-group-item">{{ $serie->name }}</li>
 <?php endforeach; ?>
 </ul>
 
