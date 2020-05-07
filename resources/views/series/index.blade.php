26619a69c1f5dc86b5cb6cb075113a4079e3ed29
Matheus Oliveira da Silva <matheus.oliveira.silv@gmail.com>
2020-05-07 09:17:02 -0300

    Fix trash icon from serie index

--- resources/views/series/index.blade.php
+++ resources/views/series/index.blade.php
@@ -19,10 +19,16 @@ Series
 @foreach ($series as $serie)
 <li class="list-group-item d-flex justify-content-between align-items-center">
     {{ $serie->name }}
+
+    <span class="d-flex">
+        <a href="/series/{{ $serie->id }}/seasons" class="btn btn-info btn-sm mr-1">
+	    <i class="fas fa-external-link-alt"></i>
+	</a>
+    </span>
     <form method="post" action="/series/remove/{{ $serie->id }}" onsubmit="return confirm('Are you sure you want to delete the {{ addslashes( $serie->name) }}?')">
         @csrf
 	@method('DELETE')
-        <button class="btn btn-danger btn-sm"><i class="far fa-trash-alt"></i></button>
+        <button class="btn btn-danger btn-sm"><i class="fas fa-trash-alt"></i></button>
     </form>
 </li>
 @endforeach
