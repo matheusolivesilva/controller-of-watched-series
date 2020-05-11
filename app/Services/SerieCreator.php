ca6786faa54710e4bc8518df8fb947530001ddbc
Matheus Oliveira da Silva <matheus.oliveira.silv@gmail.com>
2020-05-11 00:25:50 -0300

    Refactor SerieCreator Service, use transactions

--- app/Services/SerieCreator.php
+++ app/Services/SerieCreator.php
@@ -1,24 +1,38 @@
-<?php
+<?php 
 
-namespace App\Services;
+namespace App\Services; 
 
 use App\Serie;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Facade\DB; 
 
 class SerieCreator
 {
-    public function createSerie(string $serieName, int $nmrSeason, int $epBySeason): Serie
+    public function createSerie(
+        string $serieName, 
+	int $nmrSeason, 
+	int $epBySeason
+    ): Serie
     {
-        $serie = Serie::create(['name' => $serieName]);
-	$nmrSeasons = $nmrSeason;
-	for ($i = 1; $i <= $nmrSeasons; $i++) {
-	    $season = $serie->seasons()->create(['number' => $i]);
+        $serie = null;
+        DB::beginTransaction();
+	$serie = Serie::create(['name' => $serieName]);
+	$this->createSeasons($nmrSeason, $epBySeason, $serie);
+	DB::commit();
+    }
 
-	    for ($j = 1; $j <= $epBySeason; $j++) {
-	        $season->episodes()->create(['number' => $j]);
-	    }
+    private function createSeasons(int $nmrSeasons, int $epBySeason, Serie $serie)
+    {
+        for ($i = 0; $i <= $nmrSeasons; $i++) {
+            $season = $serie->seasons()->create(['number' => $i]);
+	    $this->createEpisodes($epBySeason, $season);
 	}
+    }
 
-	return $serie;
-
+    private function createEpisodes(int $epBySeason, $season): void
+    {
+        for ($j = 1; $j <= $epBySeason; $j++) {
+            $season->episodes()->create(['number' => $j]);
+	}
     }
 }
