97adef88b1b82f1671c443a0a293a6f1f6dc886a
Matheus Oliveira da Silva <matheus.oliveira.silv@gmail.com>
2020-05-11 19:52:42 -0300

    Fix SerieCreator, it wasn't returning a serie

--- app/Services/SerieCreator.php
+++ app/Services/SerieCreator.php
@@ -4,7 +4,7 @@ namespace App\Services;
 
 use App\Serie;
 use Illuminate\Database\Eloquent\Model;
-use Illuminate\Support\Facade\DB; 
+use Illuminate\Support\Facades\DB; 
 
 class SerieCreator
 {
@@ -19,6 +19,7 @@ class SerieCreator
 	$serie = Serie::create(['name' => $serieName]);
 	$this->createSeasons($nmrSeason, $epBySeason, $serie);
 	DB::commit();
+	return $serie;
     }
 
     private function createSeasons(int $nmrSeasons, int $epBySeason, Serie $serie)
