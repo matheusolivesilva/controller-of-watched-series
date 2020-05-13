4bc418866236f4cf6fc4325bd5685d2061db2cac
Matheus Oliveira da Silva <matheus.oliveira.silv@gmail.com>
2020-05-13 09:11:13 -0300

    Change seasons index, create badge for episodes

--- resources/views/seasons/index.blade.php
+++ resources/views/seasons/index.blade.php
@@ -7,8 +7,13 @@
 @section('content')
     <ul class="list-group">
         @foreach($seasons as $season)
-	    <li class="list-group-item">
-	        Season {{ $season->number }}
+	    <li class="list-group-item d-flex justify-content-between align-items-center">
+	        <a href="#"> 
+	            Season {{ $season->number }}
+		</a>
+		<span class="badge badge-secondary">
+		    	{{ $season->episodes->count() }}
+		</span>
 	    </li>
 	@endforeach
     </ul>
